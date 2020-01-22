0aeda575c10590ca9cec7c3d4cd6b4f6af9aa028
neelesh-nirmal <neelesh.nirmal@guavus.com>
2020-01-22 15:25:20 +0530

    removes todos

--- format-common/src/main/java/co/cask/hydrator/format/OrcToStructuredTransformer.java
+++ format-common/src/main/java/co/cask/hydrator/format/OrcToStructuredTransformer.java
@@ -114,7 +114,6 @@ public class OrcToStructuredTransformer extends RecordConverter<OrcStruct, Struc
     }
   }
 
-  // TODO: add array support
   private static Schema toSchema(TypeDescription schema) {
     List<Schema.Field> fields = Lists.newArrayList();
     List<String> fieldNames = schema.getFieldNames();
@@ -132,7 +131,6 @@ public class OrcToStructuredTransformer extends RecordConverter<OrcStruct, Struc
     return Schema.recordOf("record", fields);
   }
 
-  // TODO: add array support
   private static Schema getType(TypeDescription typeDescription) {
     switch (typeDescription.getCategory()) {
       case BOOLEAN:
